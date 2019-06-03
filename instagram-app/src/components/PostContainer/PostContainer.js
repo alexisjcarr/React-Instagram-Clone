24aa8ab88858fb45eb5192383e695c10ce46fc8b
alexisjcarr <acarr59@gmail.com>
2019-06-03 17:42:25 -0500

    Added comment icon

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./PostContainer.scss";
-
 import CommentSection from "../CommentSection/CommentSection";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment as faCommentRegular } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
 
 const PostContainer = props => {
   return(
@@ -9,7 +12,7 @@ const PostContainer = props => {
           <span className="user"><img className="profileImg" src={props.post.thumbnailUrl} alt="profileimg" /> <p className="username">{props.post.username}</p></span>
           <img src={props.post.imageUrl} alt="postImg" />
           <br />
-          <span className="icons">&#9825;</span>
+          <span className="icons"> <FontAwesomeIcon icon={faHeartRegular} />&nbsp;&nbsp;&nbsp;<FontAwesomeIcon icon={faCommentRegular} /></span>
           <br />
           <span className="likes">{props.post.likes} likes</span>
           <CommentSection comments={props.post.comments} />
