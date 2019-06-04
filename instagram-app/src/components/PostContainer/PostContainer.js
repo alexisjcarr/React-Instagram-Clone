cb6eec1e2874a406b2b30d1427d94262311219a8
alexisjcarr <acarr59@gmail.com>
2019-06-04 14:48:57 -0500

    Like button functional

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,25 +1,84 @@
-import React from "react";
+import React, { Component } from "react";
 import "./PostContainer.scss";
 import CommentSection from "../CommentSection/CommentSection";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons";
 
-import * as moment from 'moment';
+import * as moment from "moment";
 
-const PostContainer = props => {
-  return(
+class PostContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: this.props.post.likes,
+      pressed: false
+    };
+  }
+
+  toggleLike = () => {
+    if(this.state.pressed===false) {
+      this.setState(prevState => {
+        return {
+          likes: prevState.likes + 1,
+          pressed: true,
+        }
+      });
+    } else {
+      this.setState(prevState => {
+        return {
+          likes: prevState.likes - 1,
+          pressed: false,
+        }
+      })
+    }
+  };
+
+  // toggleTask = id => {
+  //   this.setState(prevState => {
+  //     return {
+  //       todoArray: prevState.todoArray.map(todoItem => {
+  //         if (todoItem.id === id) {
+  //           return {
+  //             ...todoItem,
+  //             completed: !todoItem.completed
+  //           };
+  //         } else {
+  //           return todoItem;
+  //         }
+  //       })
+  //     };
+  //   });
+  // };
+
+  render() {
+    return (
       <div className="postContainer">
-          <span className="user"><img className="profileImg" src={props.post.thumbnailUrl} alt="profileimg" /> <p className="username">{props.post.username}</p></span>
-          <img src={props.post.imageUrl} alt="postImg" />
-          <br />
-          <span className="icons"> <FontAwesomeIcon icon={faHeart} />&nbsp;&nbsp;&nbsp;<FontAwesomeIcon icon={faComment} /></span>
-          <br />
-          <span className="likes">{props.post.likes} likes</span>
-          <CommentSection comments={props.post.comments} />
-          <p className="time">{moment(props.post.timestamp, "LLL").fromNow()}</p>
+        <span className="user">
+          <img
+            className="profileImg"
+            src={this.props.post.thumbnailUrl}
+            alt="profileimg"
+          />{" "}
+          <p className="username">{this.props.post.username}</p>
+        </span>
+        <img src={this.props.post.imageUrl} alt="postImg" />
+        <br />
+        <span className="icons">
+          {" "}
+          <span onClick={this.toggleLike}><FontAwesomeIcon icon={faHeart} /></span>
+          &nbsp;&nbsp;&nbsp;
+          <FontAwesomeIcon icon={faComment} />
+        </span>
+        <br />
+        <span className="likes">{this.state.likes} likes</span>
+        <CommentSection comments={this.props.post.comments} />
+        <p className="time">
+          {moment(this.props.post.timestamp, "LLL").fromNow()}
+        </p>
       </div>
-  );
-};
+    );
+  }
+}
 
 export default PostContainer;
