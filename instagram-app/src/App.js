2eb7d1f872b2897a4abb559e8ddfe365dea50177
alexisjcarr <acarr59@gmail.com>
2019-06-03 20:12:07 -0500

    fuggedaboutit. Added localStorage though

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -15,6 +15,45 @@ class App extends Component {
     };
   }
 
+  saveStateToLocalStorage = () => {
+    for (let key in this.state) {
+      localStorage.setItem(key, JSON.stringify(this.state[key]));
+    }
+  };
+
+  hydrateStateWithLocalStorage = () => {
+    for (let key in this.state) {
+      if (localStorage.hasOwnProperty(key)) {
+        let value = localStorage.getItem(key);
+
+        try {
+          value = JSON.parse(value);
+          this.setState({ [key]: value });
+        } catch (e) {
+          this.setState({ [key]: value });
+        }
+      }
+    }
+  };
+
+  componentDidMount = () => {
+    this.hydrateStateWithLocalStorage();
+
+    window.addEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+  };
+
+  componentWillUnmount = () => {
+    window.removeEventListener(
+      "beforeunload",
+      this.saveStateToLocalStorage.bind(this)
+    );
+
+    this.saveStateToLocalStorage();
+  };
+
   render() {
     return (
       <div className="App">
