6b8ea285a1daafc428795849eeb4d20f175bd287
alexisjcarr <acarr59@gmail.com>
2019-06-05 15:41:08 -0500

    Form saving stuff to local storage

--- instagram-app/src/components/Login/Login.js
+++ instagram-app/src/components/Login/Login.js
@@ -1,33 +1,48 @@
-import React, { Component } from 'react';
-import './Login.scss';
+import React, { Component } from "react";
+import "./Login.scss";
 
 class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: ''
-        }
-    }
-    render() { 
-        return (
-            <form>
-                <input 
-                    type="text"
-                    name="username"
-                    placeholder="Phone number, username, or email"
-                />
-                <br />
-                <input 
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                />
-                <br />
-                <button>Login</button>
-            </form>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleLoginSubmit = e => {
+    const username = this.state.username;
+    localStorage.setItem("username", username);
+    window.location.reload();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleLoginSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Phone number, username, or email"
+          onChange={this.handleChange}
+        />
+        <br />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={this.handleChange}
+        />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
 }
- 
+
 export default Login;
